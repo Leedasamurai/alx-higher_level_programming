75076db1b9debbb281346df0d0390d82f730c084
Leedasamurai <lesegoleemac@gmail.com>
2023-10-31 16:59:18 -0700

    skip

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for alph in range(97, 123):
+    if chr(alph) != 'q' and chr(alph) != 'e':
+        print("{}".format(chr(alph)), end="")
+# program that prints the ASCII alphabet, in lowercase
