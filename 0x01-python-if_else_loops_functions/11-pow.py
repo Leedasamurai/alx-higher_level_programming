278085b5d2b15989e9cf7a5342ceb7f74a47a146
Leedasamurai <lesegoleemac@gmail.com>
2023-10-31 18:26:58 -0700

    mul

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-     return (a ** b)
-#function that computes a to the power of b and return the value.
+    return (a ** b)
+# function that computes a to the power of b and return the value.
