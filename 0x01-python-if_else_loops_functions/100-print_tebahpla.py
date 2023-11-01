1ba5a6a2b0c5ff7d0c020895f07bcf5c9ee8f4ed
Leedasamurai <lesegoleemac@gmail.com>
2023-10-31 19:22:20 -0700

    rev

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,4 @@ rev = 0
 for r in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(r - rev)), end="")
     rev = 32 if rev == 0 else 0
-
-print()
-# in reverse order, alternating lowercase and uppercase 
+# in reverse order, alternating lowercase and uppercase
