3e4a2538c0a5b5be96d50de17174c001825f6130
Leedasamurai <lesegoleemac@gmail.com>
2023-10-31 17:22:21 -0700

    comb num

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,5 @@
 for num in range(0,99):
     print("{:02d}".format(num), end=", ")
 print("{:d}".format(num + 1))
-# program that prints numbers from 0 to 99 
+
+# program that prints numbers from 0 to 99.
