06c2ca3adf7275a41059b93050a51f43f356ab9b
Leedasamurai <lesegoleemac@gmail.com>
2023-11-02 16:44:51 -0700

    argum

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    arg = len(sys.argv) - 1
+
+    if arg == 0:
+        print("{} arguments.".format(arg))
+    elif arg == 1:
+        print("{} arguments:".format(arg))
+    else:
+        print("{} arguments:".format(arg))
+
+    if arg >= 1:
+        
+        arg = 0
+
+        for args in sys.argv:
+
+            if arg != 0:
+                print("{}: {}".format(arg, args))
+            arg += 1
