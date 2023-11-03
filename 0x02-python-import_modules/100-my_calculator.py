e73fe29d11240b6bfd73e039dc500001cceb2982
Leedasamurai <lesegoleemac@gmail.com>
2023-11-02 17:23:17 -0700

    opera

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    argn = len(sys.argv) - 1
+
+    if argn != 3:
+
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    opera = sys.argv[2]
+
+    if opera != '+' and opera != '-' and opera != '*' and opera != '/':
+
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if opera == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif opera == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif opera == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
+
