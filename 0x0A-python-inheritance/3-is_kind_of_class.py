ee5a76038a6b57360e0c100b36048566491f7bd5
Leedasamurai <lesegoleemac@gmail.com>
2023-12-04 17:07:04 -0800

    aclsskndi

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module with method is_kind_of_class"""
+
+def is_kind_of_class(obj, a_class):
+    """Check if the object is an instance of, or inherited from, the specified class."""
+
+    return isinstance(obj, a_class)
