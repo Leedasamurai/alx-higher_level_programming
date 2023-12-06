d3a8ab158d07319fe102950e5514020c9e33d25b
Leedasamurai <lesegoleemac@gmail.com>
2023-12-05 17:46:29 -0800

    StUdentnOnj

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module documentation goes here.
+"""
+
+class Student:
+    """
+    Student class with public instance attributes and methods for JSON representation.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): A list of strings specifying attribute names to be retrieved.
+                          If None, retrieve all attributes.
+
+        Returns:
+            dict: The dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance based on a provided dictionary.
+
+        Args:
+            json (dict): The dictionary containing attribute names and values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
