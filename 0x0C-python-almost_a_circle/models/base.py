a08d880c7d0b6b58f1eb7e0dcc778fa08545ccfb
Leedasamurai <lesegoleemac@gmail.com>
2023-12-11 18:01:59 -0800

    UpDatedres

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for Base class."""
 import json
+import csv
 
 class Base:
     """A Base class."""
@@ -99,3 +100,48 @@ class Base:
                 return instances
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Save a list of instances to a CSV file.
+
+        Args:
+            list_objs (list): List of instances.
+
+        Returns:
+            None
+        """
+        filename = "{}.csv".format(cls.__name__)
+        with open(filename, "w", newline='') as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary().values())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Load a list of instances from a CSV file.
+
+        Returns:
+            list: List of instances.
+        """
+        filename = "{}.csv".format(cls.__name__)
+        try:
+            with open(filename, "r") as file:
+                reader = csv.reader(file)
+                dictionaries = [cls.to_dictionary_from_csv(row) for row in reader]
+                instances = [cls.create(**d) for d in dictionaries]
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def to_dictionary_from_csv(row):
+        """Convert a CSV row to a dictionary.
+
+        Args:
+            row (list): CSV row.
+
+        Returns:
+            dict: Dictionary representation of the CSV row.
+        """
+        return {key: int(value) for key, value in zip(["id", "width", "height", "x", "y"], row)}    
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,14 @@
 import unittest
 import json
-from models.base import Base  # Adjust the import based on your actual project structure
+from models.base import Base
 
 class TestBase(unittest.TestCase):
+
 
     def test_constructor_with_id(self):
         instance = Base(42)
         self.assertEqual(instance.id, 42)
+
 
     def test_constructor_without_id(self):
         instance1 = Base()
@@ -14,9 +16,11 @@ class TestBase(unittest.TestCase):
         self.assertEqual(instance1.id, 1)
         self.assertEqual(instance2.id, 2)
 
+
     def test_to_json_string_empty_list(self):
         result = Base.to_json_string([])
         self.assertEqual(result, "[]")
+
 
     def test_to_json_string_non_empty_list(self):
         data = [{"key": "value"}, {"key2": "value2"}]
@@ -24,11 +28,13 @@ class TestBase(unittest.TestCase):
         expected_result = json.dumps(data)
         self.assertEqual(result, expected_result)
 
+
     def test_save_to_file_with_list_objs_none(self):
         Base.save_to_file(None)
         with open("Base.json", "r") as file:
             result = file.read()
         self.assertEqual(result, "[]")
+
 
     def test_save_to_file_with_list_objs_non_empty(self):
         # Assuming you have a class with a to_dictionary method
@@ -40,12 +46,15 @@ class TestBase(unittest.TestCase):
         Base.save_to_file(list_objs)
         with open("Base.json", "r") as file:
             result = file.read()
-        expected_result = Base.to_json_string([obj.to_dictionary() for obj in list_objs])
+        expected_result = Base.to_json_string([obj.to_dictionary() for
+obj in list_objs])
         self.assertEqual(result, expected_result)
+
 
     def test_from_json_string_empty_string(self):
         result = Base.from_json_string("")
         self.assertEqual(result, [])
+
 
     def test_from_json_string_non_empty_string(self):
         data = '[{"key": "value"}, {"key2": "value2"}]'
@@ -53,38 +62,43 @@ class TestBase(unittest.TestCase):
         expected_result = json.loads(data)
         self.assertEqual(result, expected_result)
 
+
     def test_create_rectangle(self):
         # Assuming you have a Rectangle class with an update method
         class MockRectangle:
             def __init__(self, width, height):
                 self.width = width
                 self.height = height
+
+
             def update(self, **kwargs):
                 self.__dict__.update(kwargs)
 
-        result = Base.create(Rectangle=MockRectangle, width=10, height=20)
+        result = Base.create(Rectangle = MockRectangle, width = 10,
+height = 20)
         self.assertIsInstance(result, MockRectangle)
         self.assertEqual(result.width, 10)
         self.assertEqual(result.height, 20)
+
 
     def test_create_square(self):
         # Assuming you have a Square class with an update method
         class MockSquare:
             def __init__(self, size):
                 self.size = size
+
+
             def update(self, **kwargs):
                 self.__dict__.update(kwargs)
 
-        result = Base.create(Square=MockSquare, size=5)
+        result = Base.create(Square = MockSquare, size = 5)
         self.assertIsInstance(result, MockSquare)
         self.assertEqual(result.size, 5)
+
 
     def test_load_from_file_file_not_found(self):
         result = Base.load_from_file()
         self.assertEqual(result, [])
 
-    # Add more test cases as needed
-
 if __name__ == '__main__':
     unittest.main()
-
