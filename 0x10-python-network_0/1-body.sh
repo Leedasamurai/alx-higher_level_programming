22d8041008fef827952581e76f7a8bc47148a042
Leedasamurai <lesegoleemac@gmail.com>
2024-02-29 15:45:59 -0800

    newbody

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Checks if the HTTP status code is 200 and displays the body of the response.
-
 if [ "$(curl -L -s -o /dev/null -w "%{http_code}" "$1")" = "200" ]; then
     curl -Ls "$1"
 fi
