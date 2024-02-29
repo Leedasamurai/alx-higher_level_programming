ad1c11d7b58c1b797a6578b4b554af2765dceaa6
Leedasamurai <lesegoleemac@gmail.com>
2024-02-29 15:47:16 -0800

    newbody

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Checks if the HTTP status code is 200 and displays the body of the response.
-if [ "$(curl -L -s -o /dev/null -w "%{http_code}" "$1")" = "200" ]; then
-    curl -Ls "$1"
-fi
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
