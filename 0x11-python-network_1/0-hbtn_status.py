f38b7186672bff52bb2272491d7b308d2ee19d86
Leedasamurai <lesegoleemac@gmail.com>
2024-03-01 15:16:55 -0800

    fetchweb

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status using urllib package
+It prints the body of the response, formatted according to requirements.
+"""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
